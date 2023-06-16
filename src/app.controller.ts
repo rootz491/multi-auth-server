dc44575395c4ce6e81a18739d297a5b4ccf19fcf
rootz491 <karansh491@gmail.com>
2023-06-16 23:36:26 +0530

    more api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,14 +3,13 @@ import { AppService } from './app.service';
 import { APIGuard, ApiIdentifier } from './auth/auth.guard';
 
 @Controller()
-@UseGuards(APIGuard)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   //! CASE 1 : API Identifier is incorrect
   //  In this case, Guard will deny the access to API
   @Get('/1')
-  // @UseGuards(APIGuard)
+  @UseGuards(APIGuard)
   @ApiIdentifier('hello')
   example1(): string {
     return this.appService.success();
@@ -19,7 +18,7 @@ export class AppController {
   //! CASE 2 : API Identifier is not provided
   //  In this case, Guard will deny the access to API
   @Get('/2')
-  // @UseGuards(APIGuard)
+  @UseGuards(APIGuard)
   example2(): string {
     return this.appService.success();
   }
@@ -27,9 +26,16 @@ export class AppController {
   //* CASE 3 : API Identifier is correct
   //  In this case, Guard will allow the access to API If VALUE is correct
   @Get('/3')
-  // @UseGuards(APIGuard)
-  @ApiIdentifier('analytics')
+  @UseGuards(APIGuard)
+  @ApiIdentifier('cron')
   example3(): string {
+    return this.appService.success();
+  }
+
+  @Get('/4')
+  @UseGuards(APIGuard)
+  @ApiIdentifier('logger')
+  example4(): string {
     return this.appService.success();
   }
 }
